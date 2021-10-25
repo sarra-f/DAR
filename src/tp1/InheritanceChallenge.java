370873ad5ef50768cbf4ae0d5a9bb56f343f9173
sarra-f <60540432+sarra-f@users.noreply.github.com>
2021-10-25 09:21:39 +0100

    Update InheritanceChallenge.java

--- src/tp1/InheritanceChallenge.java
+++ src/tp1/InheritanceChallenge.java
@@ -15,11 +15,11 @@ public class InheritanceChallenge {
 //
 //        //let the challenge begin : which ones are right ?
 //        object1=object2;
-//        object1=object3;
-//        object3=object1;
-//        object4=object5;
+//        object1=object3;  
+//        object3=object1 ;la classe fille C ne peut pas contenir la classe mère A
+//        object4=object5;   de même B est est la classe fille de A
 //        object3=(C)object1;
 //        object4=(B)object5;
-//        object3=(B)object5;
+//        object3=(B)object5; la classe mère A ne peut pas être affecter à la classe fille C
     }
 }
