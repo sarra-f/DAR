e7d18a7345f0a5c7e0d21edb30a2b4673a0a41e1
sarra-f <60540432+sarra-f@users.noreply.github.com>
2021-10-25 09:17:31 +0100

    Update Triangle.java

--- src/tp1/shapes/Triangle.java
+++ src/tp1/shapes/Triangle.java
@@ -1,14 +1,28 @@
 package tp1.shapes;
 
+import java.lang.Math;
+
+
 public class Triangle {
-    //attributes
+    public int a;
+    public int b;
+    public int c;
 
     //methods
-    public void perimeter(){
-        System.out.println("Perimeter : ");
+    public void perimeter(int a,int b,int c){
+        this.a=a;
+        this.b=b;
+        this.c=c;
+        int p=a+b+c;
+        System.out.println("Perimeter : "+p);
     }
 
-    public void surface(){
-        System.out.println("Perimeter : ");
+    public void surface(int a,int b, int c){
+        double p;
+        this.a=a;
+        this.b=b;
+        this.c=c;
+        p=(a+b+c)/2;
+        double s=Math.sqrt(p*((p-a)*(p-b)*(p-c)));
+        System.out.println("Surface : "+s);
     }
-}
