32bdb3ba8c28ebf79937f51d8ec89e7944df802f
sarra-f <60540432+sarra-f@users.noreply.github.com>
2021-12-16 14:50:58 +0100

    Update Client.java

--- src/tp5/Client.java
+++ src/tp5/Client.java
@@ -1,6 +1,30 @@
 package tp5;
 
 import java.rmi.Naming;
+package TP5;
+
+import java.rmi.Naming;
+
+public class Client {
+
+    public static void main(String[] args) {
+        try {
+            MyInterface service1 = (MyInterface) Naming.lookup("rmi://127.0.0.1:1250/server");
+            System.out.println(service1.sayHello());
+	    MyInterface service2 = (MyInterface) Naming.lookup("rmi://127.0.0.1:1250/server");
+            System.out.println(service2.toUpper("sarra"));
+ 	    MyInterface service3 = (MyInterface) Naming.lookup("rmi://127.0.0.1:1250/server");
+            System.out.println(service3.index("sarra","k"));
+ 	    MyInterface service4 = (MyInterface) Naming.lookup("rmi://127.0.0.1:1250/server");
+            System.out.println(service4.palindrome("azza"));
+
+        } catch (Exception e) {
+            System.out.println("Error");
+            System.out.println(e);
+        }
+    }
+}
+
 
 public class Client {
 
