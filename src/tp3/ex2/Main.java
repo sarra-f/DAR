871211ef8c46f7457b571c5a6cc0e94165e1a17e
sarra-f <60540432+sarra-f@users.noreply.github.com>
2021-12-16 14:21:26 +0100

    Update Main.java

--- src/tp3/ex2/Main.java
+++ src/tp3/ex2/Main.java
@@ -1,18 +1,19 @@
-package tp3.ex2;
+package ex2;
 
 public class Main {
 
-    // 1 - implement static counter and run it multiple times
-    // is the output always correct ? if not correct it
-    // 2 - implement my counter and run it multiple times
-    // is the output always correct ? if not correct it
+        // 1 - implement static counter and run it multiple times
+        // is the output always correct ? if not correct it
+        // 2 - implement my counter and run it multiple times
+        // is the output always correct ? if not correct it
 
-    public static void main(String[] args) {
-        MyCounter myCounter = new MyCounter();
-        MyThread t1 = new MyThread(myCounter);
-        MyThread t2 = new MyThread(myCounter);
+        public static void main(String[] args) {
+            MyCounter myCounter = new MyCounter();
+            MyThread t1 = new MyThread(myCounter);
+            MyThread t2 = new MyThread(myCounter);
 
-        t1.start();
-        t2.start();
-    }
+            t1.start();
+            t2.start();
+        }
+
 }
