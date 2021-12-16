ab8f8ffff2297572bd99fe13388e0b394cb35623
sarra-f <60540432+sarra-f@users.noreply.github.com>
2021-12-16 14:22:02 +0100

    Update MyCounter.java

--- src/tp3/ex2/MyCounter.java
+++ src/tp3/ex2/MyCounter.java
@@ -1,9 +1,11 @@
-package tp3.ex2;
+package ex2;
 
 public class MyCounter {
-
     //todo : implement me
     public void count(){
-        //print the first 500 numbers and then print 'finish !!'
+        for(int i=1;i<=500;i++){
+            System.out.println(i);
+        }
+        System.out.println("Finished!");
     }
 }
