a17c621f614e5f3c0067a3d0e168c08d92501352
sarra-f <60540432+sarra-f@users.noreply.github.com>
2021-12-16 14:45:02 +0100

    Update Server.java

--- src/tp5/Server.java
+++ src/tp5/Server.java
@@ -1,4 +1,4 @@
-package tp5;
+package TP5;
 
 import java.net.MalformedURLException;
 import java.rmi.Naming;
@@ -12,6 +12,35 @@ public class Server extends UnicastRemoteObject implements MyInterface {
     public String sayHello() {
         return "Hello World !!";
     }
+
+    @Override
+    public String toUpper(String s) throws RemoteException {
+        return s.toUpperCase();
+    }
+
+    @Override
+    public int index(String s,String a) throws RemoteException {
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == a.charAt(0)) {
+                return i;
+
+            }
+        } return -1;
+    }
+	
+	@Override
+	public boolean palindrome (String x) throws RemoteException{
+        	int i=0;
+        	for(int j=0;i<x.length()/2;i++){
+            		if((x.charAt(j))!=(x.charAt(x.length()-j-1)))
+                		x++;
+       		 }
+       		 if (i==0)
+            		return true;
+        	else
+            		return false;
+
 
     public static void main(String[] args) throws RemoteException, MalformedURLException {
         Server server = new Server();
